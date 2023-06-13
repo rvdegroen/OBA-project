3af612d2d41cf1ebb473ab11403ec21709d91d60
Whitneyas <whitneyserwaa@gmail.com>
2023-06-13 19:23:55 +0200

    chat icon clickable

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,21 +1,29 @@
-const searchButton = document.getElementById('searchButton');
-searchButton.addEventListener('click', async () => {
-    const input = document.getElementById('query');
+// const searchButton = document.getElementById('searchButton');
+// searchButton.addEventListener('click', async () => {
+//     const input = document.getElementById('query');
 
-    const response = await fetch('/api/search', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ query: input.value }),
-    });
+//     const response = await fetch('/api/search', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ query: input.value }),
+//     });
 
-    if (response.ok) {
-        const responseData = await response.json();
-        // PUT FRONTEND JAVASCRIPT CODE HERE
-        // Show books in browser
-    } else {
-        const errorMessage = await response.text();
-        console.error(errorMessage);
-    }
+//     if (response.ok) {
+//         const responseData = await response.json();
+//         // PUT FRONTEND JAVASCRIPT CODE HERE
+//         // Show books in browser
+//     } else {
+//         const errorMessage = await response.text();
+//         console.error(errorMessage);
+//     }
+// });
+
+console.log('JavaScript code is running');
+const chatIcon = document.getElementById('chatIcon');
+const chatBox = document.getElementById('chatBox');
+
+chatIcon.addEventListener('click', function () {
+    chatBox.style.display = (chatBox.style.display === 'none') ? 'block' : 'none';
 });
