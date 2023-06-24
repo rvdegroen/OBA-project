31fa7c58d73fd94df5956073a97b0f4bff511046
rvdegroen <rvdegroen@gmail.com>
2023-06-24 16:21:50 +0200

    fixed errors

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,37 +1,35 @@
 // imports
 import * as assistantImage from './assistantImage.js';
-import {
-    initializeFAQ
-} from './faq.js';
-import {
-    renderResults
-} from './renderResults.js';
+import { initializeFAQ } from './faq.js';
+import { renderResults } from './renderResults.js';
 
 const searchButton = document.getElementById('searchButton');
 
 initializeFAQ();
 
-searchButton.addEventListener('click', async () => {
-    const input = document.getElementById('query');
+if (searchButton) {
+    searchButton.addEventListener('click', async () => {
+        const input = document.getElementById('query');
 
-    const response = await fetch('/api/search', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            query: input.value
-        }),
+        const response = await fetch('/api/search', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                query: input.value,
+            }),
+        });
+
+        if (response.ok) {
+            const responseData = await response.json();
+            // go through the responseData array, look at the formats
+            // create a new object where the results are grouped by format
+            // and then adjust renderResults to use this
+            renderResults(responseData);
+        } else {
+            const errorMessage = await response.text();
+            console.error(errorMessage);
+        }
     });
-
-    if (response.ok) {
-        const responseData = await response.json();
-        // go through the responseData array, look at the formats
-        // create a new object where the results are grouped by format
-        // and then adjust renderResults to use this
-        renderResults(responseData);
-    } else {
-        const errorMessage = await response.text();
-        console.error(errorMessage);
-    }
-});
+}
