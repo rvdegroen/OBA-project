fbf5b2e053695115f3f84f06f5d972f84827a911
rvdegroen <rvdegroen@gmail.com>
2023-06-28 15:05:29 +0200

    fixed error for index.ejs

--- public/scripts/animationDetails.js
+++ public/scripts/animationDetails.js
@@ -1,35 +1,36 @@
 export function displayMessages() {
-    const messageWrapper = document.querySelector(".message-wrapper");
-    const messageList = document.getElementById("message-list");
+    const messageWrapper = document.querySelector('.message-wrapper');
+    const messageList = document.getElementById('message-list');
     const messages = [
-        "Hier vind je meer informatie ",
-        "Hier kan je iets opslaan als favoriet",
-        "Je favorieten vind je hier!",
-        "Hier kan je deze pagina delen?",
-        "Niet uitkomen? Praat met een medewerker!",
+        'Hier vind je meer informatie ',
+        'Hier kan je iets opslaan als favoriet',
+        'Je favorieten vind je hier!',
+        'Hier kan je deze pagina delen?',
+        'Niet uitkomen? Praat met een medewerker!',
     ];
     let index = 0;
 
     function showMessage() {
-        if (index < messages.length) {
-            const message = messages[index];
+        if (messageList) {
+            if (index < messages.length) {
+                const message = messages[index];
 
-            const li = document.createElement("li");
-            li.className = "message";
-            li.textContent = message;
+                const li = document.createElement('li');
+                li.className = 'message';
+                li.textContent = message;
 
-            messageList.innerHTML = ""; // Remove previous messages
-            messageList.appendChild(li);
+                messageList.innerHTML = ''; // Remove previous messages
+                messageList.appendChild(li);
 
-            index++;
+                index++;
 
-            setTimeout(showMessage, 3000); // Delay in milliseconds before displaying the next message
-        } else {
-            // All messages have been displayed, hide the message wrapper and stick1
-            messageWrapper.style.display = "none";
+                setTimeout(showMessage, 3000); // Delay in milliseconds before displaying the next message
+            } else {
+                // All messages have been displayed, hide the message wrapper and stick1
+                messageWrapper.style.display = 'none';
+            }
         }
     }
 
     showMessage();
-
 }
