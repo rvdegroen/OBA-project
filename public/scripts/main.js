47279b71b8f53d65e312f0e51c11209980a735d1
rvdegroen <rvdegroen@gmail.com>
2023-06-29 11:39:14 +0200

    added dialog

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,39 +1,21 @@
-// 
+//
 
-import {
-    initializeFAQ
-} from './faq.js';
-import {
-    renderResults
-} from './renderResults.js';
-import {
-    helpFromAssistant
-} from './helpFromAssistant.js';
-import {
-    desktopAssistantImage
-} from './helpFromAssistant.js';
-import {
-    displayChatMessages
-} from './chatsParagraph.js';
-import {
-    fadeOutAfterDelay
-} from './animation.js';
-import {
-    displayMessages
-} from './animationDetails.js';
-
-
-
-
+import { initializeFAQ } from './faq.js';
+import { renderResults } from './renderResults.js';
+import { helpFromAssistant } from './helpFromAssistant.js';
+import { desktopAssistantImage } from './helpFromAssistant.js';
+import { displayChatMessages } from './chatsParagraph.js';
+import { fadeOutAfterDelay } from './animation.js';
+import { displayMessages } from './animationDetails.js';
 
 initializeFAQ();
 desktopAssistantImage();
 
-
-
-
 const searchButton = document.getElementById('searchButton');
-
+// modal
+const dialogButton = document.getElementById('dialog-button');
+const modal = document.getElementById('modal');
+const closeButton = document.getElementById('close-button');
 
 if (searchButton) {
     searchButton.addEventListener('click', async () => {
@@ -64,17 +46,23 @@ if (searchButton) {
         }
     });
 
-
     displayChatMessages();
     fadeOutAfterDelay();
-
-
-
 }
 
-
-
 document.addEventListener('DOMContentLoaded', () => {
-
     displayMessages(); // Call the displayMessages function
+});
+
+// dialog src: https://www.youtube.com/watch?v=ywtkJkxJsdg&t=3s
+
+modal.classList.add('hidden');
+dialogButton.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    modal.showModal();
+});
+
+closeButton.addEventListener('click', () => {
+    modal.classList.add('hidden');
+    modal.close();
 });
